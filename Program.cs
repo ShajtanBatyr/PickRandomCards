27fb83cdfe6a9015530419cd402a457499a1e8e9
ShajtanBatyr <stepashka51@yandex.ru>
2022-08-06 01:44:00 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] cardArray = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cardArray)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Число не действительно");
+            }
+        }
+    }
+}
